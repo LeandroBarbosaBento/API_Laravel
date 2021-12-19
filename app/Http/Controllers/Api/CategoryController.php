d7f360db751a53167ab4c99e8aa45df917d22b29
LeandroBarbosaBento <67109002+LeandroBarbosaBento@users.noreply.github.com>
2021-12-19 12:14:12 -0300

    Configuração dos métodos do Controller e rotas para lidar com Categorias e correção no model de produto

---
+++ app/Http/Controllers/Api/CategoryController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+
+    private $category;
+
+    public function __construct(Category $category)
+    {
+        $this->category = $category;
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return $this->category->get();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        // Verificar se o nome é diferente de vazio
+        return $this->category->create($request->all());
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //return $this->category->where('id', $id)
+        //                     ->get();
+
+        // print_r($this->category->find($id));
+
+        return $this->category->find($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $category = $this->category->find($id);
+
+        $category->name = $request->name;
+
+        $category->save();
+
+        return $category;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        /**
+         * Adicionar verificação de existência de produtos
+         * Apagar produtos que são dessa categoria
+         *
+         */
+
+        $category = $this->category->find($id);
+        $category->delete();
+    }
+}
