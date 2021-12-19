a72185f951ed49fd6503c235847d83589fb6d632
LeandroBarbosaBento <67109002+LeandroBarbosaBento@users.noreply.github.com>
2021-12-19 12:18:58 -0300

    Upload de modificações no arquivo de rotas

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,11 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+/**
+ * Create all the routes for Category
+ *
+ * Run "php artisan route:list" to see all
+ */
+Route::apiResource('categories',  \App\Http\Controllers\Api\CategoryController::class);
+
